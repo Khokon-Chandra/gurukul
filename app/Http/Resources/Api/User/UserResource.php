2a6877d6aca617857a01599e6fe37c87a1c47797
Khokon-Chandra <khokonchandra4@gmail.com>
2023-11-18 16:33:05 +0600

    CKNT-35-cikanationapi-user-list | user resource | response type | search query | test response json structure | test

---
+++ app/Http/Resources/Api/User/UserResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources\Api\User;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'                => $this->id,
+            'name'              => $this->name,
+            'username'          => $this->username,
+            'type'              => $this->type,
+            'email'             => $this->email,
+            'email_verified_at' => $this->email_verified_at,
+            'active'            => $this->active,
+            'last_login_ip'     => $this->last_login_ip,
+            'last_login_at'     => $this->last_login_at,
+            'timezone'          => $this->timezone,
+            'created_at'        => $this->created_at->format('d-F-Y h:i A'),
+        ];
+    }
+}
