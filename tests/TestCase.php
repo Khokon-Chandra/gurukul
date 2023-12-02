c2f785a83920976c31af225035f1dafa9e6e71bb
Celestine Stephen <decele2011@gmail.com>
2023-12-02 11:45:34 +0100

    fix failing test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,10 @@
 
 namespace Tests;
 
-use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication, RefreshDatabase;
+    use CreatesApplication;
 }
