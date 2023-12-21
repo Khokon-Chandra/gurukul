38b9913664e9672c3c77e6c4d4089bfde285871a
Celestine Stephen <decele2011@gmail.com>
2023-12-22 00:39:18 +0100

    refactoring of permissions

--- app/Trait/Authorizable.php
+++ app/Trait/Authorizable.php
@@ -2,15 +2,11 @@
 
 namespace App\Trait;
 
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Support\Facades\Request;
-use Spatie\Permission\Models\Permission;
 
 trait Authorizable
 {
-
-    private $abilities = [];
 
     /**
      * Override of callAction to perform the authorization before.
@@ -18,50 +14,26 @@ trait Authorizable
      * @param $method
      * @param $parameters
      * @return mixed
+     * @throws AuthorizationException
      */
 
     public function callAction($method, $parameters)
     {
-        if ($ability = $this->getAbility($method)) {
-            $this->authorize($ability);
+        $abilities = config('abilities');
+        $permissions = $abilities['route_permissions'];
+
+        $routeName = Request::route()->getName();
+        $permission = $permissions[$routeName];
+
+        $unProtectedRoutes = array_merge(
+            array_map(fn ($routeName) => route($routeName), $abilities['unprotected_route_names']),
+            array_map(fn ($routeUrl) => url($routeUrl), $abilities['unprotected_route_url']),
+        );
+
+        if (! in_array(route($routeName), $unProtectedRoutes)) {
+            $this->authorize($permission['name']);
         }
 
         return parent::callAction($method, $parameters);
-    }
-
-    public function getAbility($method)
-    {
-        $routeName   = explode('.', Request::route()->getName());
-
-        $action      = Arr::get($this->getAbilities(), $method) ?? $method;
-
-        $actionIndex = count($routeName) == 1 ? 0 : (count($routeName) == 2 ? 0 : 1);
-
-        $index =  [
-            '1' => 0,
-            '2' => 0,
-            '3' => 1,
-            '4' => 2,
-            '5' => 3,
-            '6' => 4,
-            '7' => 5
-        ][count($routeName)];
-
-
-        if($action == $routeName[$index]){
-            return $action;
-        }
-
-        return $action ? $action . '_' . @$routeName[$index] : null;
-    }
-
-    private function getAbilities()
-    {
-        return config('abilities');
-    }
-
-    public function setAbilities($abilities)
-    {
-        $this->abilities = $abilities;
     }
 }
